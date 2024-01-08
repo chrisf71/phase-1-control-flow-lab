59dd015f0b1aac9b45c4600a060e614f492dffa2
chrisf71 <christopherf442@hotmail.com>
2024-01-08 21:45:36 +0000

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
   // Write your code here!
+  let message;
+  switch (true) {
+    case distance <= 400:
+      message = "This one is on me!"
+      break;
+    case distance > 400 && distance <= 2000:
+      message = "That will be twenty bucks."
+      break;
+    case distance <= 2500:
+      message = "I will gladly take your thirty bucks."
+      break;
+      case distance > 2500:
+        message = "No can do."
+        break;
+
+  }
+  return message
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  let result = city === "NYC" ? "Ok, sounds good." : "No go."
+  return result
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+
+  let message;
+
+  switch(tip) {
+    case "generous":
+      message = "Thank you so much."
+      break;
+      case "not as generous":
+        message = "Thank you."
+        break;
+        default:
+          message = "Bye."
+  }
+  return message;
 }
